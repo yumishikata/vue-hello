7120ddc883bbc273535bff50d8e0e3a23bd0c52c
Yumi Shikata <yumishika17@gmail.com>
2021-03-18 21:43:56 +0100

    Esercizio fatto

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Ciao a tutti scusate il ritardo",
+            image: "https://yt3.ggpht.com/ytc/AAUvwngGorhnfXYAJMGMZH4SKgNU7dkGguYOtihxy1yM=s900-c-k-c0x00ffffff-no-rj"
+        }
+    }
+);
+
